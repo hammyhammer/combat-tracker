d4d75cae4886f4c2a69fc40acb65b3f9b380e356
Hampton Jacobs <hammyjacobs@gmail.com>
2022-07-14 16:25:44 -0400

    quick use state test

--- client/src/screens/homepage.jsx
+++ client/src/screens/homepage.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
-import Timer from '../components/Timer'
+import Timer from '../components/Timer';
+import { useState } from 'react';
+
 export default function homepage() {
+  const [timer, setTimer] = useState(0)
+
   return (
     <div>
+
       <Timer />
       homepage
     </div>
